0212f01e6c2f0be6a991699fc723f05dcc7f2f1d
xdye <dierduarte@gmail.com>
2018-04-14 03:25:58 +0100

    First version of github queries

--- api/author_rank.py
+++ api/author_rank.py
@@ -7,7 +7,7 @@ api_token = 'a68f7ffae3ee31dd86209dcc5e56be49e9cd974a'
 headers = {'Authorization': f'token {api_token}'}
 
 def query(q):
-    return { "query": f"{q}" }
+    return { "query": q }
 
 def author_rank(username):
     return rank_followers(followers) + rank_following(following)
@@ -19,45 +19,62 @@ def rank_following(following):
     return 0
 
 if __name__ == '__main__':
+
+    queryBody = '''
+        followers {
+          totalCount
+        }
+        following {
+          totalCount
+        }
+        issuesOpen:issues(states:OPEN) {
+          totalCount
+        }
+        issuesClosed:issues(states:CLOSED) {
+          totalCount
+        }
+        organizations {
+          totalCount
+        }
+        pinnedRepositories(privacy:PUBLIC) {
+          totalCount
+        }
+        pullOpen:pullRequests(states:OPEN) {
+          totalCount
+        }
+        pullClosed:pullRequests(states:CLOSED) {
+          totalCount
+        }
+        pullMerged:pullRequests(states:MERGED) {
+          totalCount
+        }
+        repositories(privacy: PUBLIC) {
+          totalCount
+        }
+        repositoriesContributedTo(privacy: PUBLIC) {
+          totalCount
+        }
+        starredRepositories {
+          totalCount
+        }
+        watching(privacy: PUBLIC) {
+          totalCount
+        }
+        location
+        company
+        createdAt
+        avatarUrl
+        '''
+
     q = '''
-    query{
-        user(login: "faviouz"){
-            followers{
-                totalCount
-            }
-            following {
-                totalCount
-            }
-            issues{
-              totalCount
-            }
-            organizations{
-              totalCount
-            }
-            pinnedRepositories{
-              totalCount
-            }
-            pullRequests{
-              totalCount
-            }
-            repositories(privacy: PUBLIC){
-              totalCount
-            }
-            repositoriesContributedTo(privacy: PUBLIC){
-              totalCount
-            }
-            starredRepositories{
-              totalCount
-            }
-            watching(privacy: PUBLIC){
-              totalCount
-            }
-            location
-            company
-            createdAt
-            avatarUrl
-          }
-    }'''
+    {
+        user1:user(login: "dedukun"){
+            %s
+        },
+        user2:user(login: "faviouz"){
+            %s
+        }
+    }''' % (queryBody,queryBody)
 
     r = requests.post(url=url, json=query(q), headers=headers)
     print(r.text)
--- api/repository_rank.py
+++ api/repository_rank.py
@@ -3,7 +3,7 @@ import json
 
 
 url = 'https://api.github.com/graphql'
-api_token = 'abf45d6aab2ff86839f056e5a53efa787515ae28'
+api_token = 'a68f7ffae3ee31dd86209dcc5e56be49e9cd974a'
 headers = {'Authorization': f'token {api_token}'}
 
 def query(q):
@@ -19,19 +19,79 @@ def rank_following(following):
     return 0
 
 if __name__ == '__main__':
-    q = '''
-    {
-        user(login: "faviouz") {
-            followers(last: 20) {
-              edges {
-                node {
-                  login
-                  createdAt
-                }
+
+    queryBody = '''
+        createdAt
+        stargazers {
+          totalCount
+        }
+        watchers {
+          totalCount
+        }
+        forkCount
+        refs(refPrefix: "refs/") {
+          totalCount
+        }
+        master: ref(qualifiedName: "master") {
+          commit: target {
+            ... on Commit {
+              history{
+                totalCount
               }
             }
+          }
         }
-    }'''
+        pushedAt
+        diskUsage
+        deployments {
+          totalCount
+        }
+        releases {
+          totalCount
+        }
+        issueOpen: issues(states: OPEN) {
+          totalCount
+        }
+        issueClosed: issues(states: CLOSED) {
+          totalCount
+        }
+        pullOpen: pullRequests(states: OPEN) {
+          totalCount
+        }
+        pullClosed: pullRequests(states: CLOSED) {
+          totalCount
+        }
+        pullMerged: pullRequests(states: MERGED) {
+          totalCount
+        }
+        mileOpen: milestones(states: OPEN) {
+          totalCount
+        }
+        mileClosed: milestones(states: CLOSED) {
+          totalCount
+        }
+        languages(first: 5, orderBy: {field: SIZE, direction: DESC}) {
+          edges {
+            node {
+              name
+            }
+          }
+        }
+        isArchived
+        isFork
+        isMirror
+        isLocked
+        '''
+
+    q = '''
+    {
+      repo1: repository(owner: "makeorbreak-io", name: "peimi") {
+        %s
+      },
+      repo2: repository(owner: "bnan", name: "markovitter") {
+        %s
+      }
+    }''' % (queryBody,queryBody)
 
     r = requests.post(url=url, json=query(q), headers=headers)
     print(r.text)
